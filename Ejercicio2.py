33d16c5c2440b70422c35802c0096d5c41645950
Federico Caballero <98574058+fede-caballero@users.noreply.github.com>
2023-05-10 21:47:06 -0300

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,21 +1,40 @@
+#!/usr/bin/env python3
+
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Copiar el contenido de un archivo en otro')
-
-parser.add_argument('-i', '--input', type=str, required=True, help='Nombre del archivo a copiar')
-parser.add_argument('-o', '--output', type=str, required=True, help='Nombre del archivo destino.')
-
+#Descripcion de la funcionalidad del script
+parser = argparse.ArgumentParser(description='Este script sirve para copiar el contenido de un archivo existente a otro. Si el archivo destino no existe, se crea')
+#Se definen los argumentos a pedir
+parser.add_argument('-i', '--input', type=str, help='Nombre del archivo a copiar')
+parser.add_argument('-o', '--output', type=str, help='Nombre del archivo destino')
+#Se define una variable que contiene a los argumentos en el objeto parser
 args = parser.parse_args()
 
-if not os.path.isfile(args.input):
-    print(f'Error: el archivo {args.input} no existe.')
-    exit()
+#Si el archivo de origen no existe, devuelve error
+if not os.path.exists(args.input):
+    print(f"Error: {args.input} no existe")
+    exit(1)
 
-with open(args.input, 'r') as f_input:
-    content = f_input.read()
 
-with open(args.output, 'w') as f_output:
-    f_output.write(content)
+if os.path.exists(args.output):
+    overwrite = input(f"Precaucion: {args.output} existe, sobreescribir? (y/n)")
+    if overwrite.lower() == 'y' or overwrite.lower() == 'yes':
+        with open(args.input, 'r') as src_file, open(args.output, 'w') as dest_file:
+            dest_file.write(src_file.read())
+            print(f"{args.input} fue copiado a {args.output}")
 
-print(f'El archivo {args.input} fue copiado a {args.output}.')
+    else:
+        print("Operacion abortada")
+        exit()
+
+elif not os.path.exists(args.output):
+    overwrite = input(f"Precaucion: {args.output} existe, sobreescribir? (y/n)")
+    if overwrite.lower() == 'y' or overwrite.lower() == 'yes':
+        with open(args.input, 'r') as src_file, open(args.output, 'x') as dest_file:
+            dest_file.write(src_file.read())
+            print(f"{args.input} fue copiado a {args.output}")
+
+    else:
+        print("Operacion abortada")
+        exit()
